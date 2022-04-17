c041fd5c7627b053532fb2c997923f783de9b0af
Rai <raimi.rarara@gmail.com>
2022-04-17 12:46:39 +0900

    fix

--- twitter/auth_twitter_signin_redirect.ts
+++ twitter/auth_twitter_signin_redirect.ts
@@ -1,5 +1,4 @@
 import { getAuth, signInWithRedirect } from "firebase/auth";
-import Router from "next/router";
 import app from "../firebase/firebase";
 import { provider } from "./twitter_provider_create";
 
@@ -7,5 +6,4 @@ export const auth = getAuth(app);
 
 export default function twitterLogin() {
   signInWithRedirect(auth, provider);
-  Router.push("/");
 }
